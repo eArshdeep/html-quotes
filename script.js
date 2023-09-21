74261f814b4d11b84949046b406b4f17f17d179e
Arshdeep Padda <aspadda@icloud.com>
2023-09-20 20:54:22 -0700

    Added Saving of Quotes
    Quotes can now be saved. Saved quotes are saved to localStorage and recalled
    during browser reload.
    
    Fixed a bug where `data` may be set to null if localStorage.data is not set
    in `initQuotes()`;
    
    Changed `createEntry()` to take a single `Quote` object param instead of
    author and content individually.

--- script.js
+++ script.js
@@ -10,6 +10,16 @@
  * @property dateModified {String}
 */
 
+const API_URL = "https://api.quotable.io/quotes/random?limit=5";
+
+/** Quotes data loaded from API.
+ * @type Quote[] */
+var data = [];
+
+/** Ids of quotes saved by the user.
+ * @type String[] */
+var saved = [];
+
 /**
  * Tracks whether placeholder cards are rendered.
  * Set to false on when first batch of entries is rendered.
@@ -17,33 +27,55 @@
 */
 var placeholdersShown = true;
 
+/** Save button text value for a saved quote. */
+const SAVED = "Saved!";
+/** Save button text value for a unsaved quote. */
+const UNSAVED = "Save";
+
 var container = document.getElementById("container");
 
-function createEntry(author, quote) {
+/**
+ * Create and add quote entry to DOM.
+ * @param {!Quote} quote Quote data.
+ */
+function createEntry(quote) {
     let entry = document.createElement("div");
     entry.classList.add("entry");
+    entry.id = quote._id;
 
     let title = document.createElement("p");
     title.classList.add("author");
-    title.textContent = author;
+    title.textContent = quote.author;
 
     let body = document.createElement("p");
     body.classList.add("quote");
-    body.textContent = quote;
+    body.textContent = quote.content;
+
+    let saveBtn = document.createElement("button");
+    saveBtn.classList.add("save");
+    saveBtn.textContent = UNSAVED;
+    saveBtn.addEventListener("click", () => {
+        handleSave(entry, saveBtn, quote);
+    });
 
     entry.append(title);
     entry.append(body);
+    entry.append(saveBtn);
     container.append(entry);
 }
-
-const API_URL = "https://api.quotable.io/quotes/random?limit=5";
-var data = [];
 
 function initQuotes() {
     if (localStorage.getItem("data") != null)
     {
-        data = JSON.parse(localStorage.getItem("data"));
+        data = JSON.parse(localStorage.getItem("data")) || [];
         populateEntries();
+        saved = JSON.parse(localStorage.getItem("saved")) || [];
+        // Restore styles for saved entries
+        for (let i = 0; i < saved.length; i++)
+        {
+            document.getElementById(saved[i]).classList.add("saved");
+            document.querySelector(`[id='${saved[i]}'] button.save`).textContent = SAVED;
+        }
     }
     else fetchQuotes();
 }
@@ -72,7 +104,7 @@ function populateEntries(entries) {
         placeholdersShown = false;
     }
     (entries || data).forEach(entry => {
-        createEntry(entry.author, entry.content);
+        createEntry(entry);
     });
 }
 
@@ -85,13 +117,39 @@ function clearEntries() {
 function handleClear (event)
 {
     data = [];
+    saved = [];
     clearEntries();
     localStorage.removeItem("data");
+    localStorage.removeItem("saved");
 }
 
 function handleLoad (event)
 {
     fetchQuotes();
+}
+
+/**
+ * Handle quote's save button being clicked.
+ * @param {HTMLDivElement} entry Enclosing div.entry element.
+ * @param {HTMLButtonElement} saveBtn Save button.
+ * @param {Quote} quote Quote associated with entry.
+ */
+function handleSave(entry, saveBtn, quote) {
+    entry.classList.toggle("saved")
+    // Saving
+    if (saveBtn.textContent == UNSAVED)
+    {
+        saveBtn.textContent = SAVED
+        saved.push(quote._id);
+        localStorage.setItem("saved", JSON.stringify(saved));
+    }
+    // Removing
+    else
+    {
+        saveBtn.textContent = UNSAVED;
+        saved = saved.filter(id => id != quote._id)
+        localStorage.setItem("saved", JSON.stringify(saved));
+    }
 }
 
 initQuotes();
